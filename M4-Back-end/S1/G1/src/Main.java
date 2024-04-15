068ca40cead5db616df8621e3246748f09407560
Federico-Massaccesi <fede99ita26@gmail.com>
2024-04-15 16:43:28 +0200

    tentativo primo esercizio

--- M4-Back-end/S1/G1/src/Main.java
+++ M4-Back-end/S1/G1/src/Main.java
@@ -2,6 +2,43 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int x = 10;
 
+        int y= 12;
+
+        int result = moltiplicazione(x,y);
+
+        System.out.println("Il risultato della moltiplicazione è " + result);
+
+        String frase1 = "Benvenuto ";
+
+        String frase2 = "Mario";
+
+        System.out.println(concatena(frase1,frase2));
+
+        String[] arrString ={"uno","due","tre","quattro","cinque"};
+
+        String addedString= "sei";
+
+        System.out.println(pushArr(arrString,addedString));
+    }
+
+    public static int moltiplicazione(int a,int b){
+
+        return a*b;
+
+    }
+
+    public static String concatena(String x,String y){
+
+    return x+y;
+
+    }
+
+    public static String[] pushArr(String[]arr,String string){
+
+        String[] pushedArr = arr.set(int 3,string);
+
+        return pushedArr;
     }
 }

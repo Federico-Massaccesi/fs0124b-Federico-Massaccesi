ca7e10626a1195bc124bf06f1e07890aa342d6f4
Federico-Massaccesi <fede99ita26@gmail.com>
2024-04-18 15:34:08 +0200

    es g4

---
+++ M4-Back-end/S1/G4/src/es/g4/Dipendente.java
@@ -0,0 +1,46 @@
+package es.g4;
+
+public class Dipendente {
+
+    private int matricola;
+
+    private int stipendio;
+
+    private String[] dipartimento = {"PRODUZIONE","AMMINISTRAZIONE","VENDITE"};
+
+    public Dipendente(int mat, int st, String[] d){
+
+        matricola = mat;
+
+        stipendio= st;
+
+        dipartimento = d;
+
+    }
+
+    public String[] setDipartimento(String newDip){
+
+        return dipartimento = new String[] {newDip};
+    }
+
+    public int getMatricola(){
+
+        return matricola;
+
+    }
+
+    public int getStipendio(){
+
+        return stipendio;
+    }
+
+    public String getDipartimento() {
+
+         String dip = dipartimento[0];
+
+         return dip;
+    }
+
+
+
+}

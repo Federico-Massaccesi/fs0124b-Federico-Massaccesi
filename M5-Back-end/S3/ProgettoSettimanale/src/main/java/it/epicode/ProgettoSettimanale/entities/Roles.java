c1a431b9f0321079d1eecd0f3e5944e9e629f3b9
Federico-Massaccesi <fede99ita26@gmail.com>
2024-05-31 10:56:22 +0200

    first commit

---
+++ M5-Back-end/S3/ProgettoSettimanale/src/main/java/it/epicode/ProgettoSettimanale/entities/Roles.java
@@ -0,0 +1,7 @@
+package it.epicode.ProgettoSettimanale.entities;
+
+public enum Roles {
+
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ M5-Back-end/S3/ProgettoSettimanale/src/test/java/it/epicode/ProgettoSettimanale/ProgettoSettimanaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.ProgettoSettimanale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
